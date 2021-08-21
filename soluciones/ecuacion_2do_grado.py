a464b7b6a14095f5ae77fef4c8ca2d28c7b57540
leugimkm <leugim911@hotmail.com>
2021-08-20 20:51:58 -0500

    update

--- soluciones/ecuacion_2do_grado.py
+++ soluciones/ecuacion_2do_grado.py
@@ -22,6 +22,8 @@ def solucion(
     if d < 0:
         x1 = (-b + sqrt(d))/(2*a)
         x2 = (-b - sqrt(d))/(2*a)
+    elif d == 0:
+        x1 = x2 = (-b/(2*a))
     else:
         x1 = (-b + d**0.5)/(2*a)
         x2 = (-b - d**0.5)/(2*a)
@@ -33,6 +35,7 @@ class TestSolucion(TestCase):
     def test_solucion(self):
         self.assertEqual(solucion(1, -5, 6.0), (3.0, 2.0))
         self.assertEqual(solucion(2.0, -7, 3), (3.0, 0.5))
+        self.assertEqual(solucion(1, -2, 1), (1.0, 1))
         self.assertEqual(solucion(1, -2, 5), ((1+2j), (1-2j)))
 
 
