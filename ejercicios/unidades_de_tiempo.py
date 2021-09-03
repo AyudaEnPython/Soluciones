6577a08d8f8e75c70077d1aeb9754d8b72ff36ae
leugimkm <leugim911@hotmail.com>
2021-09-02 21:56:46 -0500

    minor change

--- ejercicios/unidades_de_tiempo.py
+++ ejercicios/unidades_de_tiempo.py
@@ -3,7 +3,9 @@ Leer una duración del usuario como un número de días, horas, minutos y
 segundos. Calcular y mostrar el número total de segundos representados
 por esta duración.
 """
-from unittest import main, TestCase
+import doctest
+from random import randint
+
 
 def en_segundos(tiempo: str) -> int:
     """Convierte un tiempo en segundos
@@ -17,23 +19,34 @@ def en_segundos(tiempo: str) -> int:
         u: unidad
         t: tiempo(int)
     
-    >>> en_segundos('0:0:0:1')
-    1
+    >>> en_segundos('1:0:0:0')
+    86400
+    >>> en_segundos('1:0:10:4')
+    87004
+    >>> en_segundos('2:12:46:29')
+    218789
     """
     unidades = [60*60*24, 60*60, 60, 1]
     return sum([u*t for u, t in zip(unidades, map(int, tiempo.split(":")))])
 
 
-class Test(TestCase):
+def obtener_tiempo_usuario(dias: int = 7) -> str:
+    """Retorna una cadena de forma aleatoria
 
-    def test_en_segundos(self):
-        self.assertEqual(en_segundos("1:0:0:0"), 86400)
-        self.assertEqual(en_segundos("1:0:10:4"), 87004)
-        self.assertEqual(en_segundos("2:12:46:29"), 218789)
+    :param dias: Máximo de días
+    :dias type: int
+    :return: Tiempo aleatorio
+    :rtype: str
+    """
+    return f"{randint(0, dias)}:{randint(0, 24)}:{randint(0, 12)}:{randint(0, 60)}"
+
+
+def main():
+    tiempo = obtener_tiempo_usuario()
+    print(f"Tiempo original: {tiempo}")
+    print(f"Tiempo en segundos: {en_segundos(tiempo)}")
 
 
 if __name__ == "__main__":
-    import doctest
-
     doctest.testmod()
     main()
