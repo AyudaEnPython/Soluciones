93b66fafd4af06b50bf3c0caf0d8cf491e009c34
leugimkm <leugim911@hotmail.com>
2021-09-08 18:38:41 -0500

    update

---
+++ ejercicios/recursion/recursion_1.py
@@ -0,0 +1,31 @@
+"""
+Evaluar la siguiente función con un código que tome como valor inicial
+de “n” un valor dado por el usuario (recursividad)
+
+Si n>=3 muestre el resultado de F(13)
+F(n)= 3F(n-2) - 2F(n-1)
+F(1)= 3
+F(2)= 1
+"""
+from unittest import main, TestCase
+
+def f(n):
+    if n == 1:
+        return 3
+    elif n == 2:
+        return 1
+    else:
+        return 3*f(n-2) - 2*f(n-1)
+
+
+class Test(TestCase):
+
+    def test_f(self):
+        self.assertEqual(f(3), 7)
+        self.assertEqual(f(5), 43)
+        self.assertEqual(f(8), -1091)
+        self.assertEqual(f(13), 265723)
+
+
+if __name__ == "__main__":
+    main()
