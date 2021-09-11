351b07e622724143073e7766a6ea99e410f22599
leugimkm <leugim911@hotmail.com>
2021-09-11 01:13:45 -0500

    added naive solution requiered by an exercise

--- soluciones/es_primo.py
+++ soluciones/es_primo.py
@@ -2,7 +2,43 @@ from math import sqrt
 from typing import Iterable
 from unittest import main, TestCase
 
+
+def es_primo_naive(numero: int) -> bool:
+    primo = True
+    if numero == 2 or numero == 3:
+        primo = True
+    elif numero % 2 == 0 or numero < 2:
+        primo = False
+    else:
+        for i in range(3, int(numero**0.5)+1, 2):
+            if numero % i == 0:
+                primo = False
+    return primo
+
+
 def es_primo(n: int) -> bool:
+    """Determina si el número es primo o no.
+
+    :param n: Número a verificar
+    :type n: int
+    :returns: True si es primo, False si no lo es
+    :rtype: bool
+
+    >>> es_primo_(2)
+    True
+    >>> es_primo_(1)
+    False
+    """
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def es_primo_(n: int) -> bool:
     """Verifica si el número es primo o no.
 
     :param n: Número a verificar
@@ -10,9 +46,9 @@ def es_primo(n: int) -> bool:
     :return: True si es primo, False si no lo es
     :rtype: bool
 
-    >>> es_primo(2)
+    >>> es_primo_(2)
     True
-    >>> es_primo(1)
+    >>> es_primo_(1)
     False
     """
     if 1 < n < 4:
@@ -24,19 +60,23 @@ def es_primo(n: int) -> bool:
 
 
 class Test(TestCase):
+    true_cases = [2, 3, 5, 7, 11, 13]
+    false_cases = [1, 10, 33, -19, 0, 3*5]
+    funciones = [
+        es_primo,
+        es_primo_,
+        es_primo_naive,
+    ]
 
     def test_primos(self):
-        self.assertTrue(es_primo(2))
-        self.assertTrue(es_primo(3))
-        self.assertTrue(es_primo(5))
-        self.assertTrue(es_primo(7))
-        self.assertTrue(es_primo(11))
+        for f in self.funciones:
+            for n in self.true_cases:
+                self.assertTrue(f(n))
     
     def test_no_primos(self):
-        self.assertFalse(es_primo(-19))
-        self.assertFalse(es_primo(0))
-        self.assertFalse(es_primo(1))
-        self.assertFalse(es_primo(3*5))
+        for f in self.funciones:
+            for n in self.false_cases:
+                self.assertFalse(f(n))
 
 
 if __name__ == "__main__":
