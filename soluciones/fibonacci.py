23923fa974726395601f9be18e285db3f7c41e61
leugimkm <leugim911@hotmail.com>
2021-09-14 01:34:56 -0500

    small changes

--- soluciones/fibonacci.py
+++ soluciones/fibonacci.py
@@ -8,33 +8,33 @@ fibo_itw: Fiboanacci iterativo (while)
 from unittest import main, TestCase
 
 
-def fibo_rn(n):
+def fibo_rn(n: int):
     if n <=1:
         return n
     else:
         return fibo_rn(n-1) + fibo_rn(n-2)
 
 
-def fibo_itr(n):
+def fibo_rc(n: int, cache: dict = {0:0, 1:1}):
+    if n not in cache:
+        cache[n] = fibo_rc(n-1, cache) + fibo_rc(n-2, cache)
+    return cache[n]
+
+
+def fibo_itr(n: int) -> int:
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a+b
     return a
 
 
-def fibo_itw(n):
+def fibo_itw(n: int) -> int:
     a, b = 0, 1
     i = 0
     while i < n:
         a, b = b, a+b
         i += 1
     return a
-
-
-def fibo_rc(n, cache={0:0, 1:1}):
-    if n not in cache:
-        cache[n] = fibo_rc(n-1, cache) + fibo_rc(n-2, cache)
-    return cache[n]
 
 
 class Test(TestCase):
