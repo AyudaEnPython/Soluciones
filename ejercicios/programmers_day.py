358b56406e8e9fcfd25b72f5e1ac804c3f106e6b
leugimkm <leugim911@hotmail.com>
2021-09-30 00:17:01 -0500

    minor change

--- ejercicios/programmers_day.py
+++ ejercicios/programmers_day.py
@@ -1,13 +1,9 @@
 """
-Prints "Happy Programmer's Day!" if the current date (expressed in days)
+Print "Happy Programmer's Day!" if the current date (expressed in days)
 is equal to 256.
 """
 from datetime import datetime
 
 
 current_day = datetime.now().timetuple().tm_yday
-
-if current_day == 256:
-    print("Happy Programmer's Day!")
-else:
-    print("Keep coding!")
+print("Happy Programmer's Day!" if current_day == 256 else "Keep coding!")
