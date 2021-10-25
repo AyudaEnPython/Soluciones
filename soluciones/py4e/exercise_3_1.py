07dbf0cf0641979812fa889225f0350e91c289a6
leugimkm <leugim911@hotmail.com>
2021-10-25 12:17:34 -0500

    update

--- soluciones/py4e/exercise_3_1.py
+++ soluciones/py4e/exercise_3_1.py
@@ -4,10 +4,12 @@ hora usando la entrada para calcular el salario bruto. Paga la tarifa
 por hora por las horas hasta 40 y 1.5 veces la tarifa por hora para
 todas las horas que se trabajaron por encima de las 40 horas. Use 45
 horas y una tasa de 10.50 por hora para probar el programa (la paga
-debe ser 498.75). Debes utilizar "input" para leer una cadena y "float"
-para convertir la cadena en un número. No se preocupe por la
-comprobación de errores en la entrada del usuario: suponga que el
-usuario escribe los números correctamente.
+debe ser 498.75).
+
+Debes utilizar "input" para leer una cadena y "float" para convertir la
+cadena en un número.
+No se preocupe por la comprobación de errores en la entrada del
+usuario: suponga que el usuario escribe los números correctamente.
 """
 
 horas = float(input("Horas: "))
