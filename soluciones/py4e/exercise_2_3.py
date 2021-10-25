bdf4f393e5eff903a113de3520b1e740ab572750
leugimkm <leugim911@hotmail.com>
2021-10-24 23:36:23 -0500

    update

--- soluciones/py4e/exercise_2_3.py
+++ soluciones/py4e/exercise_2_3.py
@@ -1,7 +1,12 @@
 """
-Write a program to prompt the user for hours and rate per hour using
-input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to
-test the program (the pay should be 96.25). You should use input to
-read a string and float() to convert the string to a number. Do not
-worry about error checking or bad user data.
+Escriba un programa para solicitar al usuario las horas y la tarifa por
+hora utilizando la entrada para calcular el salario bruto. Utilice 35
+horas y una tarifa de 2,75 por hora para probar el programa (el pago
+debe ser 96,25). Debe usar input para leer una cadena y float () para
+convertir la cadena en un número. No se preocupe por la verificación de
+errores o datos de usuario incorrectos.
 """
+
+horas = float(input("Ingresar horas: "))
+tarifa = float(input("Ingresar tarifa: "))
+print("Pago: ", str(horas * tarifa))
