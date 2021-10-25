39b653b20e341f1f738308811ba05b1d2f902de0
leugimkm <leugim911@hotmail.com>
2021-10-24 23:39:03 -0500

    update

--- soluciones/py4e/exercise_2_3.py
+++ soluciones/py4e/exercise_2_3.py
@@ -1,10 +1,10 @@
 """
-Escriba un programa para solicitar al usuario las horas y la tarifa por
-hora utilizando la entrada para calcular el salario bruto. Utilice 35
-horas y una tarifa de 2,75 por hora para probar el programa (el pago
-debe ser 96,25). Debe usar input para leer una cadena y float () para
-convertir la cadena en un número. No se preocupe por la verificación de
-errores o datos de usuario incorrectos.
+Escriba un programa para solicitar al usuario Horas y tarifa por hora
+utilizando la función de entrada para calcular el salario bruto. Use 35
+horas y una tasa de 2,75 por hora para probar el programa (la paga debe
+ser 96.25). Debes utilizar input () para leer una cadena y float ()
+para convertir la cadena en un número. No se preocupe por la
+comprobación de errores o los datos de usuario incorrectos.
 """
 
 horas = float(input("Ingresar horas: "))
