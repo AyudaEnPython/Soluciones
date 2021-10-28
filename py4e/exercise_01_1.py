74961fd92a18c8f2392a60e8b23f42bfcbd8a4ae
leugimkm <leugim911@hotmail.com>
2021-10-28 00:57:53 -0500

    update

---
+++ py4e/exercise_01_1.py
@@ -0,0 +1,11 @@
+"""
+Escriba un programa que use una declaración print para decir
+'hello world' como se muestra en 'Salida deseada'.
+
+    Salida deseada
+    +-------------+
+    | hello world |
+    +-------------+
+
+"""
+print("hello world")
