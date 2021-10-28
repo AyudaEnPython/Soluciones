91857c0bd16a6b42fff78f2f98ba19370dde2d5f
leugimkm <leugim911@hotmail.com>
2021-10-28 00:58:03 -0500

    minor change

--- py4e/exercise_02_3.py
+++ py4e/exercise_02_3.py
@@ -5,14 +5,14 @@ utilizando la función de entrada para calcular el salario bruto.
 Use 35 horas y una tasa de 2,75 por hora para probar el programa (la
 paga debe ser 96.25).
 
+    Salida deseada
     +-------------+
     | Paga: 96.25 |
     +-------------+
 
 Debes utilizar input () para leer una cadena y float () para convertir
-la cadena en un número.
-No se preocupe por la comprobación de errores o los datos de usuario
-incorrectos.
+la cadena en un número. No se preocupe por la comprobación de errores o
+los datos de usuario incorrectos.
 """
 
 horas = float(input("Ingresar horas: "))
