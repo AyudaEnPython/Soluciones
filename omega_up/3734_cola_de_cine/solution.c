33429897a2358a4c3862461fb117d37936a12bc5
leugimkm <leugim911@hotmail.com>
2021-11-03 23:56:05 -0500

    added c solution

---
+++ omega_up/3734_cola_de_cine/solution.c
@@ -0,0 +1,24 @@
+/**
+ *    Estatus    : AC 
+ *    Porcentaje : 100.00%
+ *    Memoria    : 1.60 MB
+ *    Tiempo     : 0.00s
+*/
+
+#include <stdio.h>
+
+int main() {
+    int m = 0, n = 0, t = 0, c = 0, p = 0;
+    while (n != -1) {
+        scanf("%i", &n);
+        c++;
+        if (c == 1) m += n;
+        t += n;
+        if (n != 0) p += n;
+        if (p > m) m = p;
+        p--;
+    }
+    t += 1;
+    printf("%i %i", t, m);
+    return 0;
+}
