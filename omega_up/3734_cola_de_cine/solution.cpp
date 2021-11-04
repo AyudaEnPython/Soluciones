ffe75d17bc6e369c154c22bed045d70cf8e3fef4
leugimkm <leugim911@hotmail.com>
2021-11-03 23:56:13 -0500

    added cpp solution

---
+++ omega_up/3734_cola_de_cine/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ *    Estatus    : AC
+ *    Porcentaje : 100.00%
+ *    Memoria    : 3.47 MB
+ *    Tiempo     : 0.00s
+*/
+
+#include <bits/stdc++.h>
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int m = 0, n = 0, t = 0, c = 0, p = 0;
+  
+  while (n != -1) {
+      std::cin >> n;
+      c++;
+      if (c == 1) m += n;
+      t += n;
+      if (n != 0) p += n; 
+      if (p > m) m = p;
+      p--;
+    }
+  t += 1;
+  std::cout << t << " " << m;
+  return 0;
+}
