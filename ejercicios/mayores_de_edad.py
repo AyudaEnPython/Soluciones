5424e4f9d6212c93b4263eef2df22469e3e63cff
leugimkm <leugim911@hotmail.com>
2021-11-12 01:26:41 -0500

    update

--- ejercicios/mayores_de_edad.py
+++ ejercicios/mayores_de_edad.py
@@ -1,5 +1,6 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 
+# ------------------------ REDACCION ORIGINAL ------------------------
 1. Crear un programa que pida un número entero e imprimirlo, si no se
 ingresa deberá preguntar otra vez por el número entero hasta que
 ingrese un número positivo.
@@ -8,6 +9,7 @@ ingrese un número positivo.
 
 3. Con la lista anterior, contar cuantos son mayores de edad.
 
+# --------------------------------------------------------------------
 NOTA: Los ejercicios estan mal redactados, el instructor deberia tener
     mas claridad e integrar lo enseñado con ejercicios bien diseñados.
 """
@@ -24,8 +26,8 @@ def ingresar_numero():
         except ValueError:
             print("No es un número entero.")
 
-print(ingresar_numero())
+print(ingresar_numero()) # 1 
 
-edades = [18, 13, 11, 19, 17, 15, 16, 14, 12, 10, 20]
+edades = [18, 13, 11, 19, 17, 15, 16, 14, 12, 10, 20] # 2
 
-print(f"Mayores de edad: {len([e for e in edades if e >= 18])} personas")
+print(f"Mayores de edad: {len([e for e in edades if e >= 18])} personas") # 3
