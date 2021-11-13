c3db2ae89f28c61767df516408a93e9346f5b365
leugimkm <leugim911@hotmail.com>
2021-11-12 22:48:39 -0500

    minor changes

--- ejercicios/plot_funcion.py
+++ ejercicios/plot_funcion.py
@@ -1,8 +1,13 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 
+# ------------------------ Enunciado original ------------------------
 Genere un programa que reciba una función cualquiera de una variable y
 un intervalo de graficación para generar un archivo PNG con la gráfica
 resultante.
+# --------------------------------------------------------------------
+
+Nota: El enunciado es muy general, debería ser más específico el
+    instructor en cuanto a la elaboración del ejercicio.
 """
 from matplotlib import pyplot as plt
 
