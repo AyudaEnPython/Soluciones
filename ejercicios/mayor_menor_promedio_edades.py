1c0311fa7103ca6662289e78a4b9fefeefcb82db
leugimkm <leugim911@hotmail.com>
2021-11-18 00:52:59 -0500

    update

---
+++ ejercicios/mayor_menor_promedio_edades.py
@@ -0,0 +1,56 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+def sol_a():
+    # pip install prototools
+    from prototools.entradas import entrada_int
+    
+    n = entrada_int("Ingrese el número de personas: ",  min=1)
+    edades = [
+        entrada_int("Ingrese la edad: ", min=1, max=100) for _ in range(n)
+    ]
+    
+    print(f"Mayor edad: {max(edades)}")
+    print(f"Menor edad: {min(edades)}")
+    print(f"Promedio de edades: {sum(edades) / len(edades)}")
+
+
+def sol_b():
+    n = int(input("Ingrese el número de personas: "))
+    edades = [int(input("Ingrese la edad: ")) for _ in range(n)]
+    print(f"Mayor edad: {max(edades)}")
+    print(f"Menor edad: {min(edades)}")
+    print(f"Promedio de edades: {sum(edades) / len(edades)}")
+
+
+def sol_c():
+    n = int(input("Ingrese el número de personas: "))
+    edades = []
+    for _ in range(n):
+        edades.append(int(input("Ingrese la edad: ")))
+    print(f"Mayor edad: {max(edades)}")
+    print(f"Menor edad: {min(edades)}")
+    print(f"Promedio de edades: {sum(edades) / len(edades)}")
+
+
+def sol_d():
+    edades = []
+    while True:
+        try:
+            edad = int(input("Ingrese la edad: "))
+            if edad > 0:
+                edades.append(edad)
+            else:
+                break
+        except ValueError:
+            pass
+    print(f"Mayor edad: {max(edades)}")
+    print(f"Menor edad: {min(edades)}")
+    print(f"Promedio de edades: {int(sum(edades) / len(edades))}")
+
+
+# and more...
+
+
+if __name__ == "__main__":
+    sol_a()
