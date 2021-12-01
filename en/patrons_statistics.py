5478fa74fb5af76c8a07d348c05366261a7ee83e
leugimkm <leugim911@hotmail.com>
2021-12-01 03:39:46 -0500

    update

---
+++ en/patrons_statistics.py
@@ -0,0 +1,23 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Even though the group is only for spanish speakers, english
+speakers are welcome.
+"""
+
+MONTHS = (
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+)
+
+data = []
+for month in MONTHS:
+    patrons = int(input(f"Enter number of patrons for {month}: "))
+    data.append(patrons)
+
+most = max(data)
+least = min(data)
+average = sum(data) / len(data)
+
+print(f"{MONTHS[data.index(most)]} had the most patrons")
+print(f"{MONTHS[data.index(least)]} had the least patrons")
+print(f"The average monthly visitors are {average:.2f}")
