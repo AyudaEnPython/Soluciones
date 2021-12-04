f32668a246c9e3e7a3fa5c8da346c69c8f804c81
leugimkm <leugim911@hotmail.com>
2021-12-04 13:35:54 -0500

    update

---
+++ ejercicios/recursion/sumatoria.py
@@ -0,0 +1,54 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Implemente un algoritmo, usando una función recursiva, que resuelva la
+siguiente sumatoria:
+
+    K(n, p) = p + 2*p + 3*p + 4*p + ... + n*p
+
+- El programa debe pedir al usuario que ingrese un número n, y un
+    número d.
+- Luego debe calcular el valor de K(n, p) usando una función recursiva.
+- Debe imprimir el resultado de K(n, p).
+
+Algunos ejemplos del diálogo de este programa serían:
+
+    +-----------------------------------------+
+    | Input n: 5                              |
+    | Input p: 2                              |
+    | Output: 30                              |
+    +-----------------------------------------+
+
+    +-----------------------------------------+
+    | Input n: 6                              |
+    | Input p: 3                              |
+    | Output: 63                              |
+    +-----------------------------------------+
+"""
+from unittest import main, TestCase
+# pip install prototools
+from prototools import int_input
+
+
+def K(n: int, p: int) -> int:
+    if n == 1:
+        return p
+    else:
+        return n*p + K(n-1, p)
+
+
+def main_():
+    n = int_input("Input n: ")
+    p = int_input("Input p: ")
+    print("Output:", K(n, p))
+
+
+class Test(TestCase):
+
+    def test_K(self):
+        self.assertEqual(K(5, 2), 30)
+        self.assertEqual(K(6, 3), 63)
+
+
+if __name__ == '__main__':
+    # main() # uncomment this line and comment the next one to run the test
+    main_()
