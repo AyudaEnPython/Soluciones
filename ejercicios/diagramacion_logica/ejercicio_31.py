5502d478d809ec574f7a75d9c600097c99c825f6
leugimkm <leugim911@hotmail.com>
2021-12-07 17:01:06 -0500

    update

---
+++ ejercicios/diagramacion_logica/ejercicio_31.py
@@ -0,0 +1,16 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+mayor = menor = 0
+
+n = int(input("Ingrese cantidad de personas: "))
+
+for _ in range(n+1):
+    edad = int(input("Ingrese edad: "))
+    if edad >= 18:
+        mayor += 1
+    else:
+        menor += 1
+
+print(f"Mayores de edad: {mayor}")
+print(f"Menores de edad: {menor}")
