31e85e6dd1e6c810d07c1c18d66f0a80bc9fa948
leugimkm <leugim911@hotmail.com>
2021-12-07 16:03:58 -0500

    update

--- ejercicios/diagramacion_logica/ejercicio_25.py
+++ ejercicios/diagramacion_logica/ejercicio_25.py
@@ -1,7 +1,19 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
 
-n = int(input("Ingrese un número: "))
+# del ejercicio 21
+while True:
+    try:
+        n = input("Ingrese un número: ")
+        if len(n) != 5:
+            print("El número debe tener 5 dígitos")
+            continue
+        else:
+            n = int(n)
+            break
+    except ValueError:
+        print("Error: debe ingresar un número entero")
+        continue
 
 k = n 
 x = 0
