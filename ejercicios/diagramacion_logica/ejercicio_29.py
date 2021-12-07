743b1e0763a389d02119441088f43d35d9057639
leugimkm <leugim911@hotmail.com>
2021-12-07 15:23:55 -0500

    update

---
+++ ejercicios/diagramacion_logica/ejercicio_29.py
@@ -0,0 +1,7 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+for i in range(1, 6):
+    print(f"{i}^{2} = {i**2}")
+    print(f"{i}^{3} = {i**3}")
+    print(f"{i}^{4} = {i**4}\n")
