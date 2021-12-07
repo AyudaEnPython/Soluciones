0aec34426d18b684f041ac0dbd3d89264c7b9580
leugimkm <leugim911@hotmail.com>
2021-12-07 15:32:06 -0500

    update

---
+++ ejercicios/diagramacion_logica/ejercicio_20.py
@@ -0,0 +1,13 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+from random import randint
+
+n = randint(0, 36)
+
+if 10 <= n < 20:
+    print("primera")
+elif 20 <= n < 30:
+    print("segunda")
+elif 30 <= n < 40:
+    print("tercera")
