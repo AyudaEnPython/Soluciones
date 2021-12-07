8931ea6c6705f691e8676b4943670868f3bdb9ee
leugimkm <leugim911@hotmail.com>
2021-12-07 16:10:17 -0500

    update

---
+++ ejercicios/diagramacion_logica/ejercicio_26.py
@@ -0,0 +1,23 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+# del repositorio del grupo AyudaEnPython:
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/ecuacion_2do_grado.py
+
+from cmath import sqrt
+
+a = float(input("Ingrese el coeficiente a: "))
+b = float(input("Ingrese el coeficiente b: "))
+c = float(input("Ingrese el coeficiente c: "))
+
+d = b*b - 4*a*c
+if d < 0:
+    x1 = (-b + sqrt(d))/(2*a)
+    x2 = (-b - sqrt(d))/(2*a)
+elif d == 0:
+    x1 = x2 = (-b/(2*a))
+else:
+    x1 = (-b + d**0.5)/(2*a)
+    x2 = (-b - d**0.5)/(2*a)
+
+print("X1: ", x1)
+print("X2: ", x2)
