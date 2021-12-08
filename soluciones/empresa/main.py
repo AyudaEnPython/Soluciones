5ffed21c8430df3356f77969a08493408a42bd7d
leugimkm <leugim911@hotmail.com>
2021-12-08 03:31:48 -0500

    update

--- soluciones/empresa/main.py
+++ soluciones/empresa/main.py
@@ -1,25 +1,31 @@
 # pip install prototools
 from prototools import Menu
+from prototools.colorize import magenta, yellow, red, green, cyan
 from empresa import Empresa
 
 
 def main():
     empresa = Empresa()
     menu = Menu(
-        "Ayuda En Python",
-        "Gestión de Empleados",
+        yellow("Ayuda En Python"),
+        prologue_text=cyan("Gestión de Empleados"),
         exit_option_text="Salir",
+        exit_option_color=red,
     )
     menu.add_options(
-        ("Dar de alta a emplado", lambda: print()),
-        ("Aumentar sueldo a empleado", empresa.aumentar_sueldo),
-        ("Lista de empleados", lambda: print()),
-        ("Buscar empleado", lambda: print()),
-        ("Mostrar empleados por tipo", lambda: print()),
-        ("Asignar supervisor a empleado", lambda: print()),
+        (yellow("Dar de alta a emplado"), empresa.agregar_empleado),
+        (yellow("Aumentar sueldo a empleado"), empresa.aumentar_sueldo),
+        (yellow("Lista de empleados"), empresa.mostrar_empleados),
+        (yellow("Buscar empleado"), empresa.buscar_empleado),
+        (yellow("Mostrar empleados por tipo"), empresa.mostrar_por_tipo),
+        (yellow("Asignar supervisor a empleado"), empresa.asignar_supervisor),
     )
     menu.settings(
         style="double",
+        color=magenta,
+        options_color=green,
+        header_bottom=True,
+        separators=True,
     )
     menu.run()
 
