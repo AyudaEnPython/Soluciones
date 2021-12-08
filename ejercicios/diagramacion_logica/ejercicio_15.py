70c3da4e1c58f3361148d8393b0c8fec4c61cf28
leugimkm <leugim911@hotmail.com>
2021-12-07 19:24:35 -0500

    update

---
+++ ejercicios/diagramacion_logica/ejercicio_15.py
@@ -0,0 +1,10 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+a = int(input("Ingrese un número: "))
+b = int(input("Ingrese un número: "))
+
+if a % b == 0:
+    print(f"El número {a} es divisible de {b}")
+else:
+    print(f"El número {a} no es divisible de {b}")
