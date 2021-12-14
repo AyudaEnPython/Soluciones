81143ffe37ff518c3497b1263af4998d94489bea
leugimkm <leugim911@hotmail.com>
2021-12-13 22:17:06 -0500

    update

---
+++ soluciones/algo_bubble_sort.py
@@ -0,0 +1,15 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if swapped == False:
+            break
+    return arr
