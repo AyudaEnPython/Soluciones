57fee9c9e98d940b85bde6b87116dac0ed4a8de3
leugimkm <leugim911@hotmail.com>
2021-12-16 18:54:26 -0500

    update

---
+++ ejercicios/contar_vocales_ConsMayMin_digitos.py
@@ -0,0 +1,106 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Crea una función que reciba un texto y mediante una estructura
+repetitiva, lea el contenido y muestre letra por letra el contenido,
+al terminar, deberá mostrar además 2 de las siguientes acciones:
+
+- Cantidad de vocales
+- Cantidad de consonantes
+- Cantidad de mayúsculas
+- Cantidad de minúsculas
+- Cantidad de números contenidos en el texto
+
+NOTE: Se opta por ejecutar todas las acciones.
+"""
+from time import sleep
+from typing import Dict
+from unittest import main, TestCase
+# Similar al ejercicio (con variaciones mínimas):
+# https://github.com/AyudaEnPython/Soluciones/blob/main/ejercicios/contar_vocales_consonantes_digitos.py
+
+
+def contar(frase: str) -> Dict[str, int]:
+    """Cuenta vocales, consonantes, mayúsculas, minúsculas y digitos de
+    una frase.
+
+    :param frase: La frase a contar.
+    :frase type: str
+    :return: Un diccionario con las cantidades de vocales, consonantes
+        y digitos.
+    :rtype: Dict[str, int]
+    """
+    frase: str = frase
+    vocales: int = 0
+    consonantes: int = 0
+    mayusculas: int = 0
+    minusculas: int = 0
+    digitos: int = 0
+    for caracter in frase:
+        if caracter in "aeiouáéíóú" or caracter in "AEIOUÁÉÍÓÚ":
+            vocales += 1
+        if caracter in "bcdfghjklmnñpqrstvwxyzBCDFGHJKLMNÑPQRSTVWXYZ":
+            consonantes += 1
+        if caracter in "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚ":
+            mayusculas += 1
+        if caracter in "abcdefghijklmnñopqrstuvwxyzáéíóú":
+            minusculas += 1
+        if caracter in "0123456789":
+            digitos += 1
+    return {
+        "vocales": vocales, "consonantes": consonantes,
+        "mayusculas": mayusculas, "minusculas": minusculas,
+        "digitos": digitos,
+    }
+
+
+def mostrar(s: str) -> None:
+    """Muestra una frase letra por letra."""
+    for c in s:
+        print(c, end="", flush=True)
+        sleep(0.5)
+    print()
+
+
+def main_():
+    frase = input("Input: ")
+    mostrar(frase)
+    resultado = contar(frase)
+    print(f"vocales: {resultado['vocales']}")
+    print(f"consonantes: {resultado['consonantes']}")
+    print(f"mayusculas: {resultado['mayusculas']}")
+    print(f"minusculas: {resultado['minusculas']}")
+    print(f"digitos: {resultado['digitos']}")
+
+
+class Test(TestCase):
+
+    def test_contar(self):
+        self.assertEqual(
+            contar("estoy programado en el 2020"),
+            {
+                "vocales": 8, "consonantes": 11,
+                "mayusculas": 0, "minusculas": 19,
+                "digitos": 4,
+            }
+        )
+        self.assertEqual(
+            contar("PRO CS1111"),
+            {
+                "vocales": 1, "consonantes": 4,
+                "mayusculas": 5, "minusculas": 0,
+                "digitos": 4,
+            }
+        )
+        self.assertEqual(
+            contar("Hola, ¿qué tal?"),
+            {
+                "vocales": 5, "consonantes": 5,
+                "mayusculas": 1, "minusculas": 9,
+                "digitos": 0,
+            }
+        )
+
+
+if __name__ == '__main__':
+    # main() # uncomment this line and comment the next one to run tests
+    main_()
---
+++ ejercicios/pago_trabajadores.py
@@ -0,0 +1,70 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Una empresa desea sistematizar el pago de sus trabajadores, para ello
+se conoce la cantidad de horas trabajadas, el tipo de trabajador y los
+años laborados. Considera lo siguiente:
+
+- El pago por hora depende del tipo de trabajador (tiempo completo o
+    tiempo parcial) por lo que crea una función que devuelva dicho
+    valor según el tipo de trabajador. Propón los montos.
+- El trabajador recibe una bonificación por los años laborados, para
+    ello realiza una función que considere 3 intervalos de años para
+    devolver el monto propuesto.
+- El pago será la cantidad de horas laboradas, multiplicada por el
+    precio, agregando el monto de la bonificación.
+- Muestra en la salida los resultados: pago por hora, bonificación,
+    pago total.
+
+NOTE: Se opta por mejorar el diseño, por ello el tipo pasa a ser un
+    diccionario.
+"""
+from unittest import main, TestCase
+# pip install prototools
+from prototools import int_input, menu_input
+
+TIPO = {"completo": 20, "parcial": 12}
+
+
+def calcular_bonificacion(año: int) -> float:
+    rango = {
+        año < 3: 1000,
+        3 <= año < 5: 1500,
+        5 <= año < 10: 2000,
+        10 <= año: 5000,
+    }
+    return rango[True]
+
+
+def monto(horas, tipo, años):
+    return horas * TIPO[tipo] + calcular_bonificacion(años)
+
+
+def main_():
+    horas = int_input("Horas trabajadas: ")
+    tipo = menu_input(tuple(TIPO.keys()), numbers=True)
+    años = int_input("Años laborados: ", min=0)
+    print(f"Pago por hora: {TIPO[tipo]}")
+    print(f"Bonificación: {calcular_bonificacion(años)}")
+    print(f"Pago total: {monto(horas, tipo, años)}")
+
+
+class Test(TestCase):
+
+    def test_tipo(self):
+        self.assertEqual(TIPO["completo"], 20)
+        self.assertEqual(TIPO["parcial"], 12)
+
+    def test_calcular_bonificacion(self):
+        self.assertEqual(calcular_bonificacion(2), 1000)
+        self.assertEqual(calcular_bonificacion(4), 1500)
+        self.assertEqual(calcular_bonificacion(9), 2000)
+        self.assertEqual(calcular_bonificacion(12), 5000)
+
+    def test_monto(self):
+        self.assertEqual(monto(120, "completo", 2), 3400)
+        self.assertEqual(monto(80, "parcial", 4), 2460)
+
+
+if __name__ == "__main__":
+    # main() # uncomment this line and comment the next one to run tests
+    main_()
