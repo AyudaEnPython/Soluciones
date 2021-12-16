0369bf6fbd3285036490ca87600f14b347691143
leugimkm <leugim911@hotmail.com>
2021-12-15 23:59:46 -0500

    added fancy settings (menu)

--- soluciones/fabrica_mermeladas/main.py
+++ soluciones/fabrica_mermeladas/main.py
@@ -7,20 +7,21 @@ from sistema.fabrica import Fabrica
 
 
 def main():
-    menu = Menu()
+    menu = Menu(
+        "Fábrica de Mermeladas",
+    )
     fabrica = Fabrica()
     menu.add_options(
-        ("Ingresar ventas",
-            fabrica.ingresar_ventas),
-        ("Mostrar resúmenes totales",
-            fabrica.totales),
-        ("Mostrar detalles por producto",
-            fabrica.detalles_por_producto),
-        ("Mostrar detalles por tipo de envase",
-            fabrica.detalles_por_tipo),
+        ("Ingresar ventas", fabrica.ingresar_ventas),
+        ("Mostrar resúmenes totales", fabrica.totales),
+        ("Mostrar detalles por producto", fabrica.detalles_por_producto),
+        ("Mostrar detalles por tipo de envase", fabrica.detalles_por_tipo),
+    )
+    menu.settings(
+        style="double",
+        header_bottom=True,
     )
     menu.run()
-
 
 if __name__ == "__main__":
     main()
