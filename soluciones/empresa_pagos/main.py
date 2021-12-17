7a9a811c01bc1ddc0285dec924a6bb22f2b33fcf
leugimkm <leugim911@hotmail.com>
2021-12-17 01:15:33 -0500

    added fancy things

--- soluciones/empresa_pagos/main.py
+++ soluciones/empresa_pagos/main.py
@@ -2,18 +2,27 @@
 """
 # pip install prototools
 from prototools import Menu
+from prototools.colorize import magenta, cyan, red
+
 from empresa import Empresa
 
 
 def main():
     empresa = Empresa()
-    menu = Menu(exit_option_text="Salir")
+    menu = Menu(
+        cyan("Menu"),
+        exit_option_text=red("Salir"),
+        exit_option_color=red,
+    )
     menu.add_options(
         ("Agregar trabajadores", empresa.agregar),
         ("Mostrar Reporte", empresa.reporte),
         ("Eliminar trabajadores", empresa.eliminar),
     )
     menu.settings(
+        style="double",
+        color=magenta,
+        options_color=cyan,
         header_bottom=True,
     )
     menu.run()
