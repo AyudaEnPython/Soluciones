ef5a2e458c0570446e8b6eb536a9965563c44439
leugimkm <leugim911@hotmail.com>
2021-12-23 00:05:52 -0500

    added docstring and tests

--- soluciones/es_par.py
+++ soluciones/es_par.py
@@ -2,15 +2,43 @@
 
 Ingresar dos números y mostrar cual de ellos es par o si ambos lo son.
 """
+from unittest import main, TestCase
 
-x = int(input("Ingrese un número: "))
-y = int(input("Ingrese otro número: "))
 
-if x % 2 == 0 and y % 2 == 0:
-    print("Ambos son pares")
-elif x % 2 == 0:
-    print("El primero es par")
-elif y % 2 == 0:
-    print("El segundo es par")
-else:
-    print("Ninguno es par")
+def es_par(numero: int) -> bool:
+    """Determina si un número es par o no.
+    
+    :param numero: Número a evaluar.
+    :type numero: int
+    :return: True si el número es par, False si no lo es.
+    :rtype: bool
+    """
+    return numero % 2 == 0
+
+
+def main_():
+    x = int(input("Ingrese un número: "))
+    y = int(input("Ingrese otro número: "))
+
+    if es_par(x) and es_par(y):
+        print("Ambos son pares")
+    elif es_par(x):
+        print("El primero es par")
+    elif es_par(y):
+        print("El segundo es par")
+    else:
+        print("Ninguno es par")
+
+
+class Test(TestCase):
+
+    def test_es_par(self):
+        self.assertTrue(es_par(2))
+        self.assertTrue(es_par(4))
+        self.assertFalse(es_par(3))
+        self.assertFalse(es_par(5))
+
+
+if __name__ == "__main__":
+    # main() # uncomment this line and comment the next one to run tests
+    main_()
