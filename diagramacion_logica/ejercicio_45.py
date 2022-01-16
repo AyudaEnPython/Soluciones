a504f7c89db3015cbc1d72dfc71cbc9852508d6f
leugimkm <leugim911@hotmail.com>
2022-01-16 00:03:17 -0500

    update

--- diagramacion_logica/ejercicio_45.py
+++ diagramacion_logica/ejercicio_45.py
@@ -1,3 +1,17 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
 
+temperaturas = []
+
+while True:
+    try:
+        t = input("Ingresar temperaturas: ").split(",")
+        t = [float(i) for i in t]
+        temperaturas.extend(t)
+        if t[0] == 0:
+            break
+    except ValueError:
+        print("Ingresar solo numeros")
+
+t = [x for x in temperaturas if 5 <= x <= 15]
+print(f"Promedio de temperaturas entre 5 y 15: {sum(t) / len(t)}")
