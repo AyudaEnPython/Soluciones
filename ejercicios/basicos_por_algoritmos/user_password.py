16fedf68ec0d1d69961e8e480d4aca39c179ba58
leugimkm <leugim911@hotmail.com>
2022-01-28 23:21:09 -0500

    minor changes

--- ejercicios/basicos_por_algoritmos/user_password.py
+++ ejercicios/basicos_por_algoritmos/user_password.py
@@ -5,9 +5,9 @@ NOTE: Se opta por cambiar algunas cosas para seguir (PEP8).
 
 print("Inicio de Sesión")
 usuario = input("Usuario: ")
-password = input("Contraseña: ")
+contraseña = input("Contraseña: ")
 
-if usuario == "administrador" and password == "123":
+if usuario == "administrador" and contraseña == "123":
     print("Bienvenido al Sistema")
 else:
     print("Usuario o contraseña incorrecta")
