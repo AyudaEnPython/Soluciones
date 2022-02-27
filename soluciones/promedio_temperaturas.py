94ed0ea8c292ccc7323817647e0cac4b3ac45829
leugimkm <leugim911@hotmail.com>
2022-02-27 18:44:16 -0500

    .

--- soluciones/promedio_temperaturas.py
+++ soluciones/promedio_temperaturas.py
@@ -31,9 +31,9 @@ datos: Dict[str, List[int]] = {
 def promedio_anual(dict_: Dict[str, List[int]]) -> Dict[str, int]:
     """Calcula el promedio anual de cada región.
     
-    :param dict_: Diccionario con los datos de las regiones.
+    :param dict_: Diccionario con los datos de las regiones
     :dict_ type: Dict[str, List[int]]
-    :return: Diccionario con los promedios anuales de cada región.
+    :return: Diccionario con los promedios anuales de cada región
     :rtype: Dict[str, int]
     """
     return {k: round(sum(v) / len(v), 2) for k, v in dict_.items()}
@@ -42,9 +42,9 @@ def promedio_anual(dict_: Dict[str, List[int]]) -> Dict[str, int]:
 def menor_temperatura(dict_: Dict[str, List[int]]) -> Tuple[str, int]:
     """Determina el menor registro de cada región.
     
-    :param dict_: Diccionario con los datos de las regiones.
+    :param dict_: Diccionario con los datos de las regiones
     :dict_ type: Dict[str, List[int]]
-    :return: Tupla con el nombre de la región y el menor registro.
+    :return: Tupla con el nombre de la región y el menor registro
     :rtype: Tuple[str, int]
     """
     return {k: min(v) for k, v in dict_.items()}
@@ -52,24 +52,26 @@ def menor_temperatura(dict_: Dict[str, List[int]]) -> Tuple[str, int]:
 def limites_anual(dict_: Dict[str, List[int]]) -> Tuple[str, int, int]:
     """Determina el menor y el mayor registro de cada región.
     
-    :param dict_: Diccionario con los datos de las regiones.
+    :param dict_: Diccionario con los datos de las regiones
     :dict_ type: Dict[str, List[int]]
     :return: Tupla con el nombre de la región, el menor registro y el
-        mayor registro.
+        mayor registro
     :rtype: Tuple[str, int, int]
     """
     return {k: (min(v), max(v)) for k, v in dict_.items()}
 
 
-def mayor_promedio_anual(dict_: Dict[str, int], regiones: Tuple[str, ...]) -> str:
+def mayor_promedio_anual(
+    dict_: Dict[str, int], regiones: Tuple[str, ...]
+) -> str:
     """Determina la región con mayor promedio anual dentro de un
     conjunto de regiones.
     
-    :param dict_: Diccionario con los promedios anuales de las regiones.
+    :param dict_: Diccionario con los promedios anuales de las regiones
     :dict_ type: Dict[str, int]
-    :param regiones: Tupla con las regiones a evaluar.
+    :param regiones: Tupla con las regiones a evaluar
     :regiones type: Tuple[str, ...]
-    :return: Nombre de la región con mayor promedio anual.
+    :return: Nombre de la región con mayor promedio anual
     :rtype: str
     """
     return max(regiones, key=dict_.get)
@@ -84,7 +86,8 @@ def main_():
     for k, v in anual.items():
         print(f"{k}: {v}")
     print(
-        f"Menor temperatura: {min(menor, key=menor.get)} {min(menor.values())}"
+        f"Menor temperatura: "
+        f"{min(menor, key=menor.get)} {min(menor.values())}"
     )
     print("Menor y mayor temperatura por región")
     for k, v in limites.items():
