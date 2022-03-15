6c09465bb921fdea4e3db2d4c44bf72e280da6e8
leugimkm <leugim911@hotmail.com>
2022-03-14 22:50:53 -0500

    update: added binet's formula

--- soluciones/serie_fibonacci.py
+++ soluciones/serie_fibonacci.py
@@ -44,16 +44,35 @@ def fibonacci_for(n: int) -> List[int]:
     return secuencia
 
 
+def fibonacci_binet(n: int) -> List[int]:
+    """Genera la secuencia de Fibonacci de 'n' elementos.
+
+    :param n: cantidad de elementos de la secuencia
+    :n type: int
+    :return: secuencia de Fibonacci
+    :rtype: List[int]
+    """
+    p = (1 + 5**0.5) / 2
+    q = 1 / p
+    return [int((p**i + q**i) / 5 ** 0.5) for i in range(n)]
+    # One-liner:
+    # return [
+    #    int((((1 + 5**0.5) / 2)** i - ((1 - 5**0.5) / 2)** i) / 5**0.5)
+    #    for i in range(n)
+    # ]
+
+
 def main_(): # comment/uncomment to switch between while and for versions
     n = int(input("Ingrese la cantidad de elementos de la secuencia: "))
     print(", ".join(str(x) for x in fibonacci_while(n)))
-    #print(", ".join(str(x) for x in fibonacci_for(n)))
+    # print(", ".join(str(x) for x in fibonacci_for(n)))
+    # print(", ".join(str(x) for x in fibonacci_binet(n)))
 
 
 class Test(TestCase):
 
     def test_functions(self):
-        for function in (fibonacci_while, fibonacci_for):
+        for function in (fibonacci_while, fibonacci_for, fibonacci_binet):
             self.assertEqual(list(function(10)), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
 
 
