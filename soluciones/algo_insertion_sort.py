f7beaa99e98581b97f11f9fba8290acf2a3105a0
leugimkm <leugim911@hotmail.com>
2022-03-16 18:40:16 -0500

    update: added algo

---
+++ soluciones/algo_insertion_sort.py
@@ -0,0 +1,13 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
