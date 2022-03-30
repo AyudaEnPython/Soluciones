52a3bf8327f87dd057d8736978f71e2dd99087f5
leugimkm <leugim911@hotmail.com>
2022-03-29 19:28:49 -0500

    update

---
+++ soluciones/algo_quicksort.py
@@ -0,0 +1,11 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
