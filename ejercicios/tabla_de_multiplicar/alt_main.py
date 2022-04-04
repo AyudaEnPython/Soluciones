f930e6bb5e604619d2fcccda7f9e0ec9870dd46a
leugimkm <leugim911@hotmail.com>
2022-04-04 00:08:41 -0500

    update

--- ejercicios/tabla_de_multiplicar/alt_main.py
+++ ejercicios/tabla_de_multiplicar/alt_main.py
@@ -1,10 +1,18 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
+from typing import List
+from prototools import main_loop, int_input, textbox, tabulate
+
+
+def tabla_de_multiplicar(n: int) -> List[List[str]]:
+    return [[f"{n}", "x", f"{i:02}", "=", f"{n*i:02}"] for i in range(1, 13)]
 
 
 def main():
-    ...
+    n = int_input("Ingrese un número: ", min=1, max=13)
+    textbox(f"Tabla de multiplicar del número {n}", width=40)
+    print(tabulate(tabla_de_multiplicar(n), headless=True))
 
 
 if __name__ == "__main__":
-    main()
+    main_loop(main)
