7cd65f3a562ef5765152575c612596e25b5ee293
leugimkm <leugim911@hotmail.com>
2022-04-05 13:48:50 -0500

    fix: typo

--- diagramacion_logica/ejercicios/ejercicio_13.py
+++ diagramacion_logica/ejercicios/ejercicio_13.py
@@ -1,7 +1,7 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
 
-celcius = float(input("Ingrese la temperatura en grados celcius: "))
-farenheit = (celcius * 9/5) + 32
+celsius = float(input("Ingrese la temperatura en grados celcius: "))
+farenheit = (celsius * 9/5) + 32
 
 print(f"La temperatura en grados farenheit es: {farenheit}")
