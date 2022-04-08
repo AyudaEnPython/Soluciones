32d09c0d22e216db453f46c8a09d3ad3a40c0b2c
leugimkm <leugim911@hotmail.com>
2022-04-08 16:25:31 -0500

    fix: typo, update: added docstring and test

--- ejercicios/list_comprehension/pares_impares.py
+++ ejercicios/list_comprehension/pares_impares.py
@@ -2,18 +2,37 @@
 
 Creá un programa que pida introducir un número y un modo. Los modos
 serán “par” o “impar”. Luego se imprimirán todos los números pares o 
-mpares (según corresponda) desde 0 hasta el número introducido.
+impares (según corresponda) desde 0 hasta el número introducido.
 """
-from random import randint
 from typing import List
 # pip install prototools
 from prototools import choice_input, int_input
 
 
 def f(n: int, m: str) -> List[int]:
+    """Devuelve una lista con los números pares o impares
+    según corresponda.
+
+    :param n: Número
+    :n type: int
+    :param m: Modo, par o impar.
+    :m type: str
+    :return: Lista de números pares o impares (dependiendo del modo)
+    :rtype: List[int]
+    >>> f(7, "par")
+    [2, 4, 6]
+    """
     return [x*2 if m == "par" else (x*2) - 1 for x in range(1, n//2 + 1)]
 
-n = int_input("n: ", lang="es")
-modos = ("par", "impar")
-modo = choice_input(modos, lang="es")
-print(f(n, modo))
+
+def main():
+    print(f(
+        int_input("n: ", lang="es"),
+        choice_input(("par", "impar"), lang="es"),
+    ))
+
+
+if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+    main()
