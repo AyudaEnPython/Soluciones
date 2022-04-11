8236a693b835bdb743f1073eec5501ce950d60a9
leugimkm <leugim911@hotmail.com>
2022-04-10 20:48:04 -0500

    update: added stdev_

--- ejercicios/media_varianza_desviacion.py
+++ ejercicios/media_varianza_desviacion.py
@@ -5,22 +5,38 @@ devuelva un diccionario con su media, varianza y desviación estándar.
 """
 from typing import Dict, List
 from unittest import main, TestCase
-from statistics import mean, pvariance, pstdev
+from statistics import mean, pvariance, pstdev, stdev
 
 
 def media_varianza_desviacion(numeros: List[float]) -> Dict[str, float]:
-    """Calcula la media, varianza y desviación estándar de una muestra
-    de números.
+    """Calcula la media, varianza y desviación estándar de una población.
 
     :param numeros: Lista de números.
     :numeros type: List[float]
-    :return: Tupla con la media, varianza y desviación estándar.
+    :return: Tupla con la media, varianza y desviación estándar de una
+        población.
     :rtype: Dict[str, float]
     """
     media = sum(numeros) / len(numeros)
     varianza = sum((x - media) ** 2 for x in numeros) / len(numeros)
     desviacion = varianza ** 0.5
     return {"media": media, "varianza": varianza, "desviacion": desviacion}
+
+
+def stdev_(numeros: List[float]) -> float:
+    """Calcula la desviación estándar de una muestra de números.
+
+    :param numeros: Lista de números.
+    :numeros type: List[float]
+    :return: Desviación estándar de la muestra.
+    :rtype: float
+    """
+    return (
+        sum(
+            (x - (sum(numeros) / len(numeros))) ** 2 for x in numeros) /
+            (len(numeros) - 1
+        )
+    ) ** 0.5
 
 
 def main_():
@@ -40,7 +56,16 @@ class Test(TestCase):
         self.assertEqual(data["varianza"], pvariance(n))
         self.assertEqual(data["desviacion"], pstdev(n))
 
+    def test_media(self):
+        n = [1, 2, 3, 4.5, 5.6, 6, 7, 8, 9, 10]
+        self.assertEqual(sum(n)/len(n), 5.61)
+
+    def test_stdev(self):
+        n = [1, 2, 3, 4.5, 5.6, 6, 7, 8, 9, 10]
+        self.assertEqual(stdev_(n), 2.9979437397434037)
+        self.assertEqual(stdev_(n), stdev(n))
+
 
 if __name__ == "__main__":
-    # main() # uncomment this line and comment the next one to run tests
-    main_()
+    main() # uncomment this line and comment the next one to run tests
+    # main_()
