d77cc5f36baa029a6c9a8a1b1a60335f5b59d452
leugimkm <leugim911@hotmail.com>
2022-04-12 11:11:21 -0500

    update

--- ejercicios/arreglos/perimetro.py
+++ ejercicios/arreglos/perimetro.py
@@ -5,8 +5,51 @@ Mostrar los numeros del perimetro y calcularlo.
 """
 from random import random
 
-arr = [[random() for _ in range(40)] for _ in range(25)]
-r_arr = list(map(list, zip(*arr)))
-perimetro = [*arr[0], *arr[-1], *r_arr[0][1:-1], *r_arr[-1][1:-1]]
-print(perimetro)
-print(sum(perimetro))
+from prototools import show_matrix
+
+
+def solver_a():
+    """
+    >>> solver_a()
+    [1, 2, 3, 4, 5, 16, 17, 18, 19, 20, 6, 11, 10, 15]
+    147
+    """
+    #arr = [[random() for _ in range(40)] for _ in range(25)]
+    arr = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+
+    ]
+    r_arr = list(map(list, zip(*arr)))
+    perimetro = [*arr[0], *arr[-1], *r_arr[0][1:-1], *r_arr[-1][1:-1]]
+    print(perimetro)
+    print(sum(perimetro))
+
+
+def solver_b():
+    arr = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+
+    ]
+    r_arr = list(map(list, zip(*arr)))
+    perimetro = [*arr[0], *arr[-1], *r_arr[0][1:-1], *r_arr[-1][1:-1]]
+    t = [[0 for _ in range(5)] for _ in range(4)]
+    t[0] = arr[0]
+    t[-1] = arr[-1]
+    for i in range(1, len(t[0]) - 1):
+        t[i][0] = r_arr[0][i]
+        t[i][-1] = r_arr[-1][i]
+    show_matrix(t)
+    print(sum(perimetro))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    # solver_a()
+    # solver_b()
