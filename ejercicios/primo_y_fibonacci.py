001e3f0d8896eaf80e929acd3274af2be32da686
leugimkm <leugim911@hotmail.com>
2022-04-14 02:16:25 -0500

    update

---
+++ ejercicios/primo_y_fibonacci.py
@@ -0,0 +1,85 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Primo y Fibonacci. Los números Fibonacci tienen la siguiente secuencia
+0, 1, 1, 2, 3, 5, 8, 13 y así sucesivamente. Realizar un programa que
+permita imprimir el n-ésimo término primo de la serie de Fibonacci.
+Imprima en la salida un número por cada caso de entrada.
+
+Ejemplo de entrada
+1
+2
+3
+4
+
+Ejemplo de salida
+2
+3
+5
+13
+"""
+from math import sqrt
+from typing import Iterable
+
+from prototools import int_input, time_functions
+
+
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/fibonacci.py
+def fibo_gen(n: int) -> int:
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/fibonacci.py
+def fibo_itw(n: int) -> int:
+    a, b = 0, 1
+    i = 0
+    while i < n:
+        a, b = b, a+b
+        i += 1
+    return a
+
+
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/es_primo.py
+def es_primo(n: int) -> bool:
+    if 1 < n < 4:
+        return True
+    elif n < 2 or not n % 2:
+        return False
+    impares: Iterable = range(3, int(sqrt(n) + 1), 2)
+    return not any(not n % i for i in impares)
+
+
+def f(n: int) -> int:
+    i = j = k = r =0
+    while j != n:
+        i += 1
+        k = fibo_itw(i)
+        if es_primo(k):
+            j += 1
+            r = k
+    return r
+
+
+def g(n: int) -> int:
+    j = 0
+    for i in fibo_gen(n**2):
+        if es_primo(i):
+            j += 1
+        if j == n:
+            return i
+
+
+def main():
+    n = int_input(min=0, lang="es")
+    print(f(n))
+    # print(g(n))
+
+
+if __name__ == "__main__":
+    main()
+    # fs = {"f": f, "g": g}
+    # time_functions(fs, args=(5), globals=globals())
+    # # 'f' took 10.6349 secs
+    # # 'g' took 7.1475 secs
---
+++ ejercicios/sin_cos_e.py
@@ -0,0 +1,78 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Calcular: sin(x), cos(x) y e^x.
+Las fórmulas matemáticas son:
+
+sin x = \sum_{n=0}^{\infty} (-1)^n / \fact{2n+1} x^(2n+1)
+cos x = \sum_{n=0}^{\infty} (-1)^n / \fact{2n} x^(2n)
+e^x = \sum_{n=0}^{\infty} (-1)^n / \fact{n}
+
+El único dato de entrada es el valor de x. La salida consiste de los
+valores aproximados a 11 decimales desde el 0 hasta el 10 inclusive.
+La salida debe estar en el formato similar al ejemplo e imprimirse con
+la cantidad de decimales especificados.
+
+Ejemplo de entrada
+0.5
+
+Ejemplo de salida
+
+n  sin(0.5)      cos(0.5)      e^0.5
+-- ------------- ------------- -------------
+00 0.50000000000 1.00000000000 1.00000000000
+01 0.47916666667 0.87500000000 1.50000000000
+02 0.47942708333 0.87760416667 1.62500000000
+03 0.47942553323 0.87758246528 1.64583333333
+04 0.47942553862 0.87758256216 1.64843750000
+05 0.47942553860 0.87758256189 1.64869791667
+06 0.47942553860 0.87758256189 1.64871961806
+07 0.47942553860 0.87758256189 1.64872116815
+08 0.47942553860 0.87758256189 1.64872126504
+09 0.47942553860 0.87758256189 1.64872127042
+10 0.47942553860 0.87758256189 1.64872127069
+
+NOTE: Las funciones son tomadas del repositorio del grupo AyudaEnPython
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/aproximacion_series_maclaurin/main.py
+"""
+from math import factorial as f
+# pip install prototools
+from prototools import Menu, float_input, tabulate, clear_screen
+
+
+def sinx(x: float, n: int) -> float:
+    return sum((-1)**k * x**(2*k + 1) / f(2*k + 1) for k in range(n + 1))
+
+
+def cosx(x: float, n: int) -> float:
+    return sum((-1)**k * x**(2*k) / f(2*k) for k in range(n + 1))
+
+
+def expx(x: float, n: int) -> float:
+    return sum((x**k) / f(k) for k in range(n + 1))
+
+
+def g():
+    x = float_input("x: ")
+    clear_screen()
+    data = [
+        [
+            f"{n:02}",
+            f"{sinx(x, n):.11f}",
+            f"{cosx(x, n):.11f}",
+            f"{expx(x, n):.11f}",
+        ]
+        for n in range(11)
+    ]
+    print(tabulate(data, headers=["n", f"sin({x})", f"cos({x})", f"e^({x})"]))
+
+
+def main():
+    menu = Menu("Aproximaciones")
+    menu.add_options(
+        ("Ingresar valor de x", g),
+    )
+    menu.run()
+
+
+if __name__ == "__main__":
+    main()
