6536466af652a23d950ecfc92cce694dcdeb65d0
leugimkm <leugim911@hotmail.com>
2022-04-26 20:59:19 -0500

    update

--- en/challenges/choosing_art.py
+++ en/challenges/choosing_art.py
@@ -17,3 +17,57 @@ art = ['1ygb','2gbw','3ybg','4mbg','5bgy','6grb','7yrg','8grm','9owy']
 # output
 > 'Artworks 2, 4, 8, 9 can be shown to client'
 """
+
+art = ["1ygb", "2gbw", "3ybg", "4mbg", "5bgy", "6grb", "7yrg", "8grm", "9owy"]
+
+
+def sol_1():   
+    cs = "b", "y", "r"
+    d = []
+    for i in art:
+        k = 0
+        for c in i[1:]:
+            if c in cs:
+                k += 1
+        if k < 2:
+            d.append(i[0])
+    return d
+
+
+def sol_2():
+    return {i[0] for i in art if len(set(i[1:]) & {"b", "y", "r"}) < 2}
+
+
+def sol_3(): # by Udo Sero 
+    favorites = {'b','y','r'}
+    artsplits = {item[0]: set(item[1:]) for item in art}
+    selections = [
+        item[0] for item in artsplits.items()
+        if len(item[1].intersection(favorites)) == 1
+    ]
+    return selections
+
+
+def sol_4(): # by Himel Sharma
+    colors = ["b","y","r"]
+    chosen_ones = []
+    for arts in art:
+        chosen = False
+        for color in colors:
+            c = []
+            if color in arts:
+                c += colors
+                c.remove(color)
+                if not any([c[0] in arts, c[1] in arts]):
+                    chosen = True
+        if chosen:
+            chosen_ones += [arts[0]]
+    return chosen_ones
+
+
+if __name__ == "__main__":
+    data = sol_1()
+    # data = sol_2()
+    # data = sol_3()
+    # data = sol_4()
+    print(f"Artworks {', '.join(data)} can be shown to client")
