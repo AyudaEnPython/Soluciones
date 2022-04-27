84ea2716308a47302261999b86e9fddf7fd97158
leugimkm <leugim911@hotmail.com>
2022-04-26 20:06:58 -0500

    update

---
+++ en/challenges/choosing_art.py
@@ -0,0 +1,19 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Even though the group is only for spanish speakers, english speakers
+are welcome.
+-----------------------------------------------------------------------
+    ** PYTHON CODE CHALLENGE - CHOOSING ART **
+
+you are selecting art for a client with special tastes. client wants
+only to be shown art with favorite colors, blue(b), yellow(y) or red(r),
+but only one color per art-piece. so if a piece has blue, it must not
+contain yellow or red write code to help you select pieces to show
+client.
+All art available has been color-coded by your assistant.
+
+art = ['1ygb','2gbw','3ybg','4mbg','5bgy','6grb','7yrg','8grm','9owy']
+
+# output
+> 'Artworks 2, 4, 8, 9 can be shown to client'
+"""
