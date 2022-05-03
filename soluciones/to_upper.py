cb860f0ac5754a63198a68fd5dbf1de77931ab08
leugimkm <leugim911@hotmail.com>
2022-05-02 21:48:43 -0500

    update: added test

--- soluciones/to_upper.py
+++ soluciones/to_upper.py
@@ -1,6 +1,18 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
+import unittest
 
 
 def to_upper(s: str) -> str:
     return "".join(chr(ord(c) ^ 0x20) for c in s)
+
+
+class Test(unittest.TestCase):
+
+    def test_to_upper(self):
+        self.assertEqual(to_upper("abc"), "ABC")
+        self.assertEqual(to_upper(""), "")
+
+
+if __name__ == '__main__':
+    unittest.main()
