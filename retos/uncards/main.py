6edc675083650a7e4e48b4c830872022e55293d1
leugimkm <leugim911@hotmail.com>
2022-05-06 21:11:29 -0500

    update: minor changes

--- retos/uncards/main.py
+++ retos/uncards/main.py
@@ -12,7 +12,7 @@ def _load_players(default_: Tuple[str, str], n: int = 0) -> Dict[str, int]:
 
 def validate(input_: str, valid: str = VALID) -> str:
     if not all(c in valid for c in input_):
-        return "!"*len(input_) # raise ValueError("Invalid characters")
+        return "!"*len(input_)
     return input_
 
 
@@ -33,10 +33,8 @@ def sol(xs: str, ys: str, d: Tuple[Tuple[str, str], str] = DEFAULTS) -> str:
 
 
 def main():
-    #U = validate(input())
-    U = "P1[@]"
-    # A = validate(input())
-    A = "CZ*[ol"
+    U = validate(input())
+    A = validate(input())
     print(sol(U, A))
 
 
