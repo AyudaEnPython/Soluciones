5f3914fb91be64f7ded92590673862dd4d2fcce8
leugimkm <leugim911@hotmail.com>
2022-05-07 01:02:12 -0500

    update: added code validation

--- soluciones/mdoo_pg_epe/utils.py
+++ soluciones/mdoo_pg_epe/utils.py
@@ -1,7 +1,18 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
+from typing import List
+# pip install prototools
 from prototools import validate_float
 
 
 def validar_notas(valor: str, min: float = 0, max: float = 20) -> float:
     return validate_float(valor, min=min, max=max)
+
+
+def validar_codigos(estudiantes: List[object], codigo: str) -> List[object]:
+    if not estudiantes:
+        return codigo
+    for estudiante in estudiantes:
+        if estudiante.codigo == codigo:
+            raise ValueError
+    return codigo
