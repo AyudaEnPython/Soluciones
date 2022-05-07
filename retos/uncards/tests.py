40240ef1c6b83b8d3f94b20ad5175dff49195cd3
leugimkm <leugim911@hotmail.com>
2022-05-07 11:20:45 -0500

    update: added more tests and a note

--- retos/uncards/tests.py
+++ retos/uncards/tests.py
@@ -1,4 +1,7 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+NOTE: If there's an invalid character it'll be like a disqualified entry.
+    That behavior could be changed if the rules were more specific.
 """
 import unittest
 
@@ -9,23 +12,25 @@ class Test(unittest.TestCase):
 
     def test_validate(self):
         self.assertEqual(validate("P1[@]CZ*[ol"), "P1[@]CZ*[ol")
-        with self.assertRaises(ValueError):
-            validate("P1[@]CZ*[ol_")
+        self.assertEqual(validate("P1[@]CZ*[ol_"), "!!!!!!!!!!!!")
 
     def test_validate_other(self):
         valid = "<=?Ad@[^"
         self.assertEqual(validate("?=A^", valid), "?=A^")
-        with self.assertRaises(ValueError):
-            validate("?=A^$_", valid)
+        self.assertEqual(validate("?=A^_", valid), "!!!!!")
 
     def test_sol(self):
         self.assertEqual(sol("P1[@]", "CZ*[ol"), "UDUDA")
+        self.assertEqual(sol("CZ*[ol", "P1[@]"), "ADADU")
         self.assertEqual(sol("ro1", "]cP"), "UUU")
+        self.assertEqual(sol("]cP", "ro1"), "AAA")
 
     def test_sol_defaults(self):
         defaults = "X", "Y", "E"
         self.assertEqual(sol("P1[@]", "CZ*[ol", defaults), "XEXEY")
+        self.assertEqual(sol("CZ*[ol", "P1[@]", defaults), "YEYEX")
         self.assertEqual(sol("ro1", "]cP", defaults), "XXX")
+        self.assertEqual(sol("]cP", "ro1", defaults), "YYY")
 
 
 if __name__ == "__main__":
