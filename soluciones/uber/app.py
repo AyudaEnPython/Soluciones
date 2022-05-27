41010b49bbbf1e4ad30ba8ba5c196ce0216c03b4
leugimkm <leugim911@hotmail.com>
2022-05-26 22:41:43 -0500

    update

--- soluciones/uber/app.py
+++ soluciones/uber/app.py
@@ -1,19 +1,23 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+NOTE: it'll be refactored later...
 """
+from random import randint
+
 from main import uber_object, registrar_vehiculo
 
 
 def main():
-    vehiculo = registrar_vehiculo("ABC123", "Toyota", "2020")
+    vehiculo = registrar_vehiculo("AEP101", "Mazda", "2022")
     uber = uber_object(vehiculo, "premium")
-    uber.set_calificacion(5)
-    uber.viaje(2)
-    uber.viaje(4)
-    uber.viaje(1)
+    uber.set_calificacion(randint(3, 10))
+    for _ in range(randint(6, 10)):
+        uber.viaje(randint(1, 20))
     uber.mostrar()
-    uber.set_calificacion(1)
+    uber.set_calificacion(randint(1, 3))
     uber.reset()
-    uber.viaje(2)
+    for _ in range(randint(1, 4)):
+        uber.viaje(randint(1, 20))
     uber.mostrar()
     uber.enviar_reparacion()
     uber.viaje(3)
