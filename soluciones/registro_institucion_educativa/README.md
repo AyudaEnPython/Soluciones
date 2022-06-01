b5783072422ec38ff4228c8773b025b2a2ade72a
leugimkm <leugim911@hotmail.com>
2022-06-01 13:23:11 -0500

    update

---
+++ soluciones/registro_institucion_educativa/README.md
@@ -0,0 +1,15 @@
+# Enunciado Original
+
+Una institución educativa necesita un programa que registre la información
+de la cantidad de estudiantes de cada uno de sus once grupos, los cuales van
+desde primero hasta undécimo año, con el fin de identificar que grupos tienen
+cupo para el próximo año, donde se sabe que el máximo de estudiantes se 35 y
+el mínimo de estudiantes es 10.
+
+El programa debe generar un informa con los siguientes resultados:
+- Lista de los grupos que tiene cupo disponible, con la cantidad de cupos
+  disponibles.
+- Lista de los grupos que pueden no ofertarse por tener un número de estudiantes
+  menor al mínimo.
+- Lista de grupos que tiene sobre cupo con la cantidad de más que tiene (ejemplo:
+  si el grupo tiene 38 estudiantes, tiene un sobre cupo de 3 estudiantes).
---
+++ soluciones/registro_institucion_educativa/main.py
@@ -0,0 +1,54 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+from string import ascii_uppercase
+from typing import Dict, List, Tuple
+# pip install prototools
+from prototools import int_input, textbox
+
+MIN, MAX = 10, 35
+GRUPOS = 3
+Data = Dict[str, int]
+
+
+def inscribir() -> None:
+    grupos: Data = {k:0 for k in ascii_uppercase[:GRUPOS]}
+    for grupo in grupos:
+        grupos[grupo] += int_input(f"[{grupo}] Cantidad de alumnos: ", min=0)
+    return grupos
+
+
+def _disponibilidad(grupos: Data, valor: int) -> List[Tuple[str, int]]:
+    return [
+        (grupo, grupos[grupo]) for grupo in grupos
+        if grupos[grupo] <= valor
+    ]
+
+
+def _sobrecupo(grupos: Data) -> List[Tuple[str, int]]:
+    return [
+        (grupo, grupos[grupo] - MAX) for grupo in grupos
+        if grupos[grupo] > MAX
+    ]
+
+
+def informe(grupos: Data) -> None:
+    textbox("Informe")
+    print("Grupos disponibles:")
+    for grupo, cantidad in _disponibilidad(grupos, MAX):
+        if cantidad > MIN:
+            print(f"Grupo {grupo} | Disponibles: {MAX - cantidad} cupos")
+    print("\nGrupos no disponibles:")
+    for grupo, cantidad in _disponibilidad(grupos, MIN):
+        print(f"Grupo {grupo} | inscritos al momento: {cantidad}")
+    print("\nGrupos sobrecupados:")
+    for grupo, cantidad in _sobrecupo(grupos):
+        print(f"Grupo {grupo} | sobre cupo: {cantidad} estudiantes")
+
+
+def main():
+    data: Data = inscribir()
+    informe(data)
+
+
+if __name__ == "__main__":
+    main()
