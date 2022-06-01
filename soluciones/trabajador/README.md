881427bb5541a3cb3554464c664fd3e7b18f7abc
leugimkm <leugim911@hotmail.com>
2022-06-01 01:43:08 -0500

    update

---
+++ soluciones/trabajador/README.md
@@ -0,0 +1,15 @@
+# Enunciado Original
+
+Crear una clase `Trabajador` que modele la información que una
+empresa mantiene sobre cada trabajador: DNI, nombres y apellidos,
+sueldo base, pago por hora extra, horas extras realizadas en el
+mes.
+
+Al crear objetos de esta clase se deberán proporcionar los datos
+de un trabajador, y los métodos deberán permitir realizar:
+
+- Cálculo del sueldo incluyendo pago de horas extras.
+- Cálculo de descuentos. Suponer 8% de impuesto a la renta y 13%
+  por seguro de desempleo.
+- Cálculo del suelo neto.
+- Mostrar detalles de pago.
---
+++ soluciones/trabajador/main.py
@@ -0,0 +1,55 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+from dataclasses import dataclass
+
+RENTA, DESEMPLEO = 0.08, 0.13
+
+
+@dataclass
+class Persona:
+    dni: str
+    nombre: str
+    apellido: str
+
+    def __str__(self) -> str:
+        return f"[{self.dni}] {self.apellido}, {self.nombre}"
+
+
+@dataclass
+class Trabajador(Persona):
+    sueldo: float
+    pago_horas_extras: float
+    horas_extras: int
+
+    def obtener_sueldo(self) -> float:
+        return self.sueldo + self.pago_horas_extras * self.horas_extras
+
+    def obtener_descuentos(self):
+        return self.obtener_sueldo() * (RENTA + DESEMPLEO)
+
+    def obtener_sueldo_neto(self):
+        return self.obtener_sueldo() - self.obtener_descuentos()
+
+    def detalles(self) -> str:
+        return (
+            f"{super().__str__()}\n"
+            f"Sueldo: {self.obtener_sueldo():.2f}\n"
+            f"Descuentos: {self.obtener_descuentos():.2f}\n"
+            f"Sueldo neto: {self.obtener_sueldo_neto():.2f}"
+        )
+
+
+def main():
+    trabajador = Trabajador(
+        dni="45478432",
+        nombre="Jason",
+        apellido="Becker",
+        sueldo=6500,
+        pago_horas_extras=80,
+        horas_extras=10,
+    )
+    print(trabajador.detalles())
+
+
+if __name__ == "__main__":
+    main()
