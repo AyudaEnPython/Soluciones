fbe355679d48d315a8e58ec4326eab98c7dc95ac
leugimkm <leugim911@hotmail.com>
2022-06-01 20:03:33 -0500

    update

---
+++ ejercicios/digitos_doble_del_producto.py
@@ -0,0 +1,11 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Usando un ciclo for, escribe el siguiente programa: todos los números
+de dos dígitos que son iguales al doble del producto de los dígitos
+que conforman el número brindado se muestran en una columna.
+"""
+
+for n in range(10, 100):
+    x, y = str(n)
+    if 2 * int(x) * int(y) == n:
+        print(n)
