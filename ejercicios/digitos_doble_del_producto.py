0ba46aab5577bad5d59181efa8d4930f9dfa622c
leugimkm <leugim911@hotmail.com>
2022-06-01 20:07:07 -0500

    u: added oneliner

--- ejercicios/digitos_doble_del_producto.py
+++ ejercicios/digitos_doble_del_producto.py
@@ -9,3 +9,6 @@ for n in range(10, 100):
     x, y = str(n)
     if 2 * int(x) * int(y) == n:
         print(n)
+
+# oneliner:
+# print(*[n for n in range(10, 100) if 2 * int(str(n)[0]) * int(str(n)[1]) == n])
