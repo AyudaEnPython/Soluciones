ce03a0f3848a28759e8b1493e672c540b2db4c1f
leugimkm <leugim911@hotmail.com>
2022-06-03 00:15:00 -0500

    update

---
+++ ejercicios/funciones/lambdas_operaciones.py
@@ -0,0 +1,42 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Realiza lo siguiente:
+
+- Codifica una función anónima o lambda que sume, reste, multiplique y
+    divida 2 números introducidos por el usuario en consola. Deberá
+    tener el siguiente comportamiento:
+
+    PROGRAMA QUE REALIZA OPERACIONES CON 2 NÚMEROS
+
+    Entrada:
+
+    Ingresa el primer número: 5
+    Ingresa el segundo número: 2
+
+    Salida:
+
+    La suma es: 7
+    La resta es: 3
+    La multiplicación es: 10
+    La división es: 2.5
+
+Nota: Recuerda que la división entre 0 está indeterminada, y esa
+    condición la debes de considerar a la hora de resolver el
+    problema.
+"""
+
+f = lambda x, y: (
+    ("suma", x + y),
+    ("resta", x - y),
+    ("multiplicación", x * y),
+    ("división", x / y),
+)
+
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+
+try:
+    for s, v in f(x, y):
+        print(f"La {s} es: {v}")
+except ZeroDivisionError:
+    print("La división entre cero está indefinida")
