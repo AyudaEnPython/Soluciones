48b8e70a58dd2a923ec5678f5b5ec477c5f0fadd
leugimkm <leugim911@hotmail.com>
2022-06-14 23:08:27 -0500

    update

---
+++ ejercicios/validar_correo/README.md
@@ -0,0 +1,52 @@
+# Enunciado Original
+
+Dada una colección de N textos que se introducen por teclado, usted
+debe contar la cantidad de correos electrónicos válidos que existen.
+Puede asumir con seguridad que nunca un correo será una sub-cadena, sino
+que siempre viene como el texto introducido completo.
+
+Una dirección email es válida si cumple los siguientes requisitos:
+
+- El nombre de usuario es válido.
+- Aparece un arroba "@" luego del nombre de usuario.
+- Tiene un dominio válido.
+
+Un nombre de usuario es válido si comienza por letras, solo incluye letras
+minúsculas (ojo que usted puede forzarla) y números, no posee caracteres
+especiales y su longitud debe estar entre 5 y 20 caracteres.
+
+Un dominio es válido si se compone de dos o más textos separados por ".",
+cada uno de esos textos tiene una longitud de 2 a 5 letras minúsculas.
+
+---
+
+Proponga una solución en Python para este problema que utilice funciones.
+A continuación se muestra un ejemplo y salida esperada en Python.
+
+Hint: 
+- Puede usar la función `find`, que retorna la posición inicial de sub-cadena
+  en el texto y -1 si no aparece:
+
+      pos = str("Hola mundo cruel").find("mundo")
+
+  `pos = 5`, pero `pos = -1` si buscamos la `"z"`.
+
+
+Entradas:
+
+    N = 5
+
+    "Hola mundo cruel"
+    "pepe234@abs.erty.es"
+    "usuariovalido1@abcdef.erty.es"
+    "juanitoperezmartinex234@cuart.ter.cl"
+    "juanito!@google.com"
+    "3nombre@uft.edu"
+
+Salida:
+
+    2
+
+
+> __**NOTA**__: El enunciado fue modificado para mejorar la legibilidad del mismo.
+> Las entradas deberían ser 6 y no 5.
---
+++ ejercicios/validar_correo/main.py
@@ -0,0 +1,68 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+from string import ascii_lowercase
+from typing import List, Tuple
+
+
+def _helper(correo: str) -> Tuple[str, str]:
+    return correo.split("@")
+
+
+def get_data() -> List[str]:
+    correos = []
+    n = int(input("Cantidad de correos a validar: "))
+    for i in range(n):
+        correos.append(input(f"[{i+1}] Ingrese un correo: "))
+    return correos
+
+
+def _get_data(filename: str) -> List[str]:
+    with open(filename, "r") as f:
+        return [line.strip() for line in f]
+
+
+def _validar_usuario(usuario: str) -> str:
+    if usuario[0] not in ascii_lowercase:
+        raise ValueError("El nombre debe comenzar con una letra")
+    if not usuario.isalnum():
+        raise ValueError("El usuario no es válido")
+    if len(usuario) < 5 or len(usuario) > 20:
+        raise ValueError("El usuario debe tener al menos 3 caracteres")
+    return usuario
+
+
+def _validar_dominio(dominio: str) -> str:
+    if not "." in dominio:
+        raise ValueError("Dominio inválido")
+    ss = dominio.split(".")
+    for s in ss:
+        if len(s) < 2 or len(s) > 5:
+            raise ValueError("Tamaño inválido de dominio")
+        if not s.isalnum():
+            raise ValueError("No puede contener caracteres especiales")
+    return dominio
+
+
+def validar_correo(correo: str) -> str:
+    if correo.find(" ") != -1:
+        raise ValueError("El correo no debe contener espacios")
+    if not "@" in correo:
+        raise ValueError("El correo no tiene @")
+    usuario, dominio = _helper(correo)
+    usuario = _validar_usuario(usuario)
+    dominio = _validar_dominio(dominio)
+    return correo
+
+
+def main():
+    # data = get_data()
+    data = _get_data("data.txt")
+    for email in data:
+        try:
+            print(validar_correo(email))
+        except ValueError as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
