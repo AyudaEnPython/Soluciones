e71e960896666e6d47c3942cc365700d75b305f8
leugimkm <leugim911@hotmail.com>
2022-06-18 00:02:25 -0500

    update

---
+++ ejercicios/analizar_licores/README.md
@@ -0,0 +1,17 @@
+# Enunciado Original
+
+Usted ha sido contratado como analista de datos para una revista de
+licores. Para el próximo capítulo de la revista le han pedido analizar
+500.000 opiniones de consumidores del portal beeradvocate.
+
+El editor le ha solicitado analizar estas opiniones y dar respuesta a
+las siguientes preguntas, para ser compartidas con el comité editorial
+de la revista:
+
+1. ¿Qué cervecería produce las cervezas más fuertes por ABV%?
+2. Si tuviera que elegir 3 cervezas para recomendar usando solo estos
+    datos, ¿cuál eligiría?
+3. ¿Cuales de los factores (aroma, sabor, apariencia, paleta) son más
+    importantes para determinar la calidad general de una cerveza?
+4. Si normalmente disfruto de una cerveza debido a su aroma y
+    apariencia, ¿qué estilo de cerveza debo probar?
---
+++ ejercicios/analizar_licores/main.py
@@ -0,0 +1,80 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
+
+URL = "https://www.beeradvocate.com/beer/?view=recent"
+CONVERT = {
+    "look": "apariencia",
+    "smell": "aroma",
+    "taste": "sabor",
+    "feel": "sensacion",
+    "overall": "puntaje",
+}
+ID = "rating_fullview_content_2"
+LABELS = {
+    "abv": "BAscore_norm",
+    "detail": "muted",
+}
+
+
+@dataclass
+class Cerveza:
+    nombre: str
+    cerveceria: str
+    estilo: str
+    abv: str
+    puntaje: float
+    aroma: str
+    sabor: str
+    sensacion: str
+    apariencia: str
+
+    def to_dict(self):
+        return self.__dict__
+
+
+def clean_up(s: str) -> Dict[str, float]:
+    return {
+        CONVERT[k]: float(v) for k, v in {k:v for k, v in (x.split(": ")
+        for x in s.split(" | "))}.items()
+    }
+
+
+def scrape(url: str) -> List[Cerveza]:
+    data = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    elements = soup.find_all("div", id=ID)
+    for element in elements:
+        names = element.find_all("a")[1]
+        types = element.find_all("a")[2]
+        companies = element.find_all("a")[3]
+        abvs = element.find("span", class_=LABELS["abv"])
+        details = element.find_all("span", class_=LABELS["detail"])[1]
+        for detail in details:
+            if not detail.startswith("r"):
+                for name, company, type_, abv in zip(
+                    names, companies, types, abvs
+                ):
+                    data.append(Cerveza(
+                        nombre=name,
+                        cerveceria=company,
+                        estilo=type_,
+                        abv=abv,
+                        **clean_up(detail),
+                    ))
+    return data
+
+
+def main():
+    cervezas = scrape(URL)
+    df = pd.DataFrame.from_records([cerveza.to_dict() for cerveza in cervezas])
+    df.to_csv("data/cervezas.csv", index=False)
+
+
+if __name__ == '__main__':
+    main()
