3f1d10dcc62fa80f7d45d4349c2812aaab567aec
leugimkm <leugim911@hotmail.com>
2022-06-21 11:55:34 -0500

    u: minor change

--- ejercicios/basico/ejercicios/problema_04.py
+++ ejercicios/basico/ejercicios/problema_04.py
@@ -1,12 +1,16 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 
 Una juguetería tiene mucho éxito en dos de sus productos: payasos y
-muñecas. Suele hace venta por correo y la empresa de logística les
-cobra por peso de cada paquete así que deben calcular el peso de los
-payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso
-pesa 112 g y cada muñeca pesa 75 g. Escribir un programa qeu lea el
-número de payasos y muñecas vendidos en el último pedido y calcule el
-peso total del paquete que será enviado.
+muñecas.
+
+Suele hace venta por correo y la empresa de logística les cobra por
+peso de cada paquete así que deben calcular el peso de los payasos y
+muñecas que saldrán en cada paquete a demanda.
+
+Cada payaso pesa 112 g y cada muñeca pesa 75 g.
+
+Escribir un programa que lea el número de payasos y muñecas vendidos en
+el último pedido y calcule el peso total del paquete que será enviado.
 """
 
 PAYASO_PESO = 112
