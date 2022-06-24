63be85b231db7ec4821f933c987e163e59547c3f
leugimkm <leugim911@hotmail.com>
2022-06-24 18:30:32 -0500

    update

--- ejercicios/control_vacacional_rappi.py
+++ ejercicios/control_vacacional_rappi.py
@@ -7,16 +7,14 @@ DIAS = ((6, 12, 20), (8, 14, 22), (10, 20, 30))
 
 
 def dias_libres(id_: int, tiempo: int) -> str:
-    for n in range(1, 4):
-        if id_ == n:
-            if 1 <= tiempo < 2:
-                _d = DIAS[id_-1][0]
-            elif 2 <= tiempo < 7:
-                _d = DIAS[id_-1][1]
-            elif tiempo >= 7:
-                _d = DIAS[id_-1][2]
-            else:
-                _d = 0
+    if 1 <= tiempo < 2:
+        _d = DIAS[id_-1][0]
+    elif 2 <= tiempo < 7:
+        _d = DIAS[id_-1][1]
+    elif tiempo >= 7:
+        _d = DIAS[id_-1][2]
+    else:
+        _d = 0
     if _d != 0:
         return f"tiene derecho a {_d} dias de vacaciones"
     return f"no tiene derecho a vacaciones"
