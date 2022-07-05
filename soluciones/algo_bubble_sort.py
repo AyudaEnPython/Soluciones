3a56d587478d27d95446fa8e05cbc9d39754ce39
leugimkm <leugim911@hotmail.com>
2022-07-04 20:47:26 -0500

    u: minor change

--- soluciones/algo_bubble_sort.py
+++ soluciones/algo_bubble_sort.py
@@ -10,6 +10,6 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-        if swapped == False:
+        if not swapped:
             break
     return arr
