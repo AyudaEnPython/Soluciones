bf07bfe2189187e932c05f83adb3c8151d972000
leugimkm <leugim911@hotmail.com>
2022-07-07 00:12:20 -0500

    update

---
+++ soluciones/typewriter.py
@@ -0,0 +1,34 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+import time
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+# pip install prototools
+from prototools.keyboard import typewrite, press
+
+TXT = "* Ayuda en Python *"
+BAR = "* "*(len(TXT)//2 + 1)
+
+def start_app(app_name: str) -> None:
+    subprocess.Popen(app_name)
+
+
+def write_to_app(txt: str) -> None:
+    time.sleep(1)
+    typewrite(BAR)
+    press("enter")
+    typewrite(txt)
+    press("enter")
+    typewrite(BAR)
+    press("enter")
+    press("\t")
+    press("\t")
+    typewrite("Version ")
+    press("num1")
+
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.map(start_app, ["notepad.exe"])
+        executor.map(write_to_app, [TXT])
+    print("Done")
