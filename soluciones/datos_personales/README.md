a1ffee99dcf57fdda9c8bf8453f30819e6881036
leugimkm <leugim911@hotmail.com>
2022-07-27 17:05:37 -0500

    update

---
+++ soluciones/datos_personales/README.md
@@ -0,0 +1,28 @@
+# Enunciado Original
+
+Utilizando funciones, realizar un sistema que permita lo siguiente:
+
+    ***************************************
+            INGRESO DE INFORMACIÓN
+    ***************************************
+        1. Ingreso de datos Personales
+        2. Consulta uno
+        3. Consultar todos
+        4. Salir
+    ***************************************
+    Ingrese una opción del sistema ->
+
+1. Crear el menú de opciones.
+2. El ingreso de datos pedirá lo siguiente: Cédula, apellidos, nombres
+  y edad. Todo se almacenará en una lista.
+3. El sistema deberá validar que no se permita ingresar cédulas repetidas;
+  en el caso de un ingreso repetido mostrará un mensaje por pantalla y
+  volverá a pedir un nuevo ingreso sólo de ese valor.
+4. La opción CONSULTA UNO, permitirá mediante el ingreso de una cédula
+  mostrar la información sólo de esa persona, en caso de no existir ese
+  registro, mostrar el mensaje correspondiente.
+5. La opción CONSULTAR TODOS, permitriá mostrar por pantalla el listado de
+  todos los registros ingresados; si no existen registros mostrar un mensaje.
+6. Sólo cuando el usuario ingrese la opción 4, el sistema deberá finalizar.
+
+> __**NOTA**__: Se optá por mejorar algunas partes del enunciado
---
+++ soluciones/datos_personales/main.py
@@ -0,0 +1,75 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+from dataclasses import dataclass
+from typing import List, Tuple
+# pip install prototools
+from prototools import Menu, str_input, date_input_dmy
+
+
+@dataclass
+class Persona:
+    cedula: str
+    apellidos: str
+    nombres: str
+    fecha_nacimiento: str
+
+    def __str__(self) -> str:
+        return (
+            f"Cédula: {self.cedula}\n"
+            f"Apellidos y Nombres: {self.apellidos}, {self.nombres}\n"
+            f"Fecha de Nacimiento: {self.fecha_nacimiento}"
+        )
+
+
+def _buscar_cedula(data: List[Persona], cedula: str) -> Tuple[bool, Persona]:
+    for persona in data:
+        if persona.cedula == cedula:
+            return True, persona
+    return False, None
+
+
+def _cedula_no_repetida(data: List[Persona]) -> str:
+    while True:
+        cedula = str_input("Cédula: ")
+        encontrado, _ = _buscar_cedula(data, cedula)
+        if not encontrado:
+            return cedula
+        print("Ya existe la cédula, ingrese otra")
+
+
+def datos_personales(data: List[Persona]) -> None:
+    cedula = _cedula_no_repetida(data)
+    apellidos = str_input("Apellidos: ")
+    nombres = str_input("Nombres: ")
+    fecha_nacimiento = date_input_dmy("Fecha de nacimiento (mm/dd/aa): ")
+    persona = Persona(cedula, apellidos, nombres, fecha_nacimiento)
+    data.append(persona)
+
+
+def buscar(data: List[Persona]) -> None:
+    cedula = str_input("Cédula: ")
+    encontrado, persona = _buscar_cedula(data, cedula)
+    if encontrado:
+        print(persona)
+    else:
+        print("No se encontró la cédula")
+
+
+def mostrar(data: List[Persona]) -> None:
+    for persona in data:
+        print(persona)
+
+
+def main():
+    data: List[Persona] = []
+    menu = Menu("Ingreso de Información")
+    menu.add_options(
+        ("Datos Personales", lambda: datos_personales(data)),
+        ("Consultar", lambda: buscar(data)),
+        ("Consultar todos", lambda: mostrar(data)),
+    )
+    menu.run()
+
+
+if __name__ == "__main__":
+    main()
