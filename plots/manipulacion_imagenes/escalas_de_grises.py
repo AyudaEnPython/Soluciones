03183c62463a4710f043182aee315f8481fdd574
leugimkm <leugim911@hotmail.com>
2022-10-09 00:12:20 -0500

    u: minor changes

--- plots/manipulacion_imagenes/escalas_de_grises.py
+++ plots/manipulacion_imagenes/escalas_de_grises.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
 img = mpimg.imread('assets/logo.png')
-R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
+R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
 img_gray = 0.2989 * R + 0.5870 * G + 0.1140 * B
 
 plt.imshow(img_gray, cmap='gray')
