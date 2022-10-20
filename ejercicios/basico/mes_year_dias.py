d6dc610f3c70586acc002f9390cc69f810e5bc02
leugimkm <leugim911@hotmail.com>
2022-10-19 22:38:05 -0500

    u: added leap year logic

--- ejercicios/basico/mes_year_dias.py
+++ ejercicios/basico/mes_year_dias.py
@@ -4,20 +4,16 @@ Solicita al usuario un número de mes al año, e imprime:
 - el nombre del mes.
 - la cantidad de dias que tiene cada mes.
 """
-MESES = {
-    "1": ("enero", 31),
-    "2": ("febrero", 28),
-    "3": ("marzo", 31),
-    "4": ("abril", 30),
-    "5": ("mayo", 31),
-    "6": ("junio", 30),
-    "7": ("julio", 31),
-    "8": ("agosto", 31),
-    "9": ("setiembre", 30),
-    "10": ("octubre", 31),
-    "11": ("noviembre", 30),
-    "12": ("diciembre", 30),
-}
-mes = input("Ingresar mes expresado en números (1-12): ")
+from datetime import date
+from calendar import monthrange
+
+YEAR = date.today().year
+_MESES = (
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "setiembre", "noviembre", "diciembre",
+)
+MESES = {x: (m, monthrange(YEAR, x)[1]) for x, m in zip(range(1, 13), _MESES)}
+
+mes = int(input("Ingresar mes expresado en números (1-12): "))
 print(f"Mes: {MESES[mes][0]}")
 print(f"Dias: {MESES[mes][1]}")
