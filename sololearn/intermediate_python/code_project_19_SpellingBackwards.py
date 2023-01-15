efcf55be1254955836035cc52f571b1ee2236ceb
leugimkm <leugim911@hotmail.com>
2023-01-14 21:58:54 -0500

    update

--- sololearn/intermediate_python/code_project_19_SpellingBackwards.py
+++ sololearn/intermediate_python/code_project_19_SpellingBackwards.py
@@ -1,2 +1,32 @@
 """
+Spelling Backwards
+==================
+
+Given a string as input, use recursion to output each letter of the
+strings in reverse order, on a new line.
+
+Sample Input:
+HELLO
+
+Sample Output:
+O
+L
+L
+E
+H
+
+Hint:
+Complete the recursive spell() function to produce the expected
+result.
 """
+
+
+def spell(txt):
+    if not txt:
+        return
+    print(txt[-1])
+    return spell(txt[:-1])
+
+
+txt = input()
+spell(txt)
