a898b18a3165358b6364b1b5f7d56502a99f6a76
leugimkm <leugim911@hotmail.com>
2023-01-17 00:35:38 -0500

    update

--- sololearn/intermediate_python/code_project_39_TitleEncoder.py
+++ sololearn/intermediate_python/code_project_39_TitleEncoder.py
@@ -1,2 +1,27 @@
 """
+Title Encoder
+=============
+
+You are given a file named "books.txt" with book tiles, each on a
+separate line.
+
+To encode the book titles you need to take the first letters of each
+word in the title and combine them.
+For example, for the book title "Games of Thrones" the encoded version
+should be "GoT".
+
+Complete the program to read the book title from the file and output
+the encoded versions, each on a new line.
+
+Hint:
+You can use the .split() method to get a list of words in a string.
 """
+
+file = open("/usercode/files/books.txt", "r")
+for line in  file.readlines():
+    print("".join([s[0] for s in line.split()]))
+file.close()
+
+# with open("/usercode/files/books.txt", "r") as f:
+#     for line in  f.readlines():
+#         print("".join([s[0] for s in line.split()]))
