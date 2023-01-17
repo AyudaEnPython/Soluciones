0dc414ced0dc063cebb38fe13501ea703e16680f
leugimkm <leugim911@hotmail.com>
2023-01-16 20:42:16 -0500

    update

--- sololearn/intermediate_python/code_project_33_RegistrationSystem.py
+++ sololearn/intermediate_python/code_project_33_RegistrationSystem.py
@@ -1,2 +1,30 @@
 """
+Registration System
+===================
+
+You are making a registration form for a website.
+The form has a name field, which should be more than 3 characters long.
+Any name that has less than 4 characters is invalid.
+
+Complete the code to take the name as input, and raise an exception if
+the name is invalid, outputing "Invalid Name". Output "Account Created"
+if the name is valid.
+
+Sample Input:
+abc
+
+Sample Output:
+Invalid Name
+
+Hint:
+Use try/raise/except statements.
 """
+
+try:
+    name = input()
+    if len(name) < 4:
+        raise Exception()
+except:
+    print("Invalid Name")
+else:
+    print("Account Created")
