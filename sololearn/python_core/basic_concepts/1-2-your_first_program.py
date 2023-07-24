4886ab994c0c61b8de96ae02130a7bcd193f56a8
leugimkm <leugim911@hotmail.com>
2023-07-24 11:18:13 -0500

    update

---
+++ sololearn/python_core/basic_concepts/1-2-your_first_program.py
@@ -0,0 +1,13 @@
+"""
+Your First Program
+
+
+Write a program to print "Python is fun".
+Note that the sentence starts with a capital letter.
+
+Hint
+Use print() function.
+
+NOTE: Remember to enclose the text into single or double quotes.
+"""
+print("Python is fun")
