befd5d89129e6f150fba14fd649911ca74929c21
leugimkm <leugim911@hotmail.com>
2023-07-24 11:17:56 -0500

    update

---
+++ sololearn/python_for_beginners/basic_concepts/2-2-goal.py
@@ -0,0 +1,11 @@
+"""
+Simple Calculation
+
+
+You need to calculate the points earned by a soccer team.
+The team won 18 games and ended 7 games as a draw.
+A win brings 3 points, while a draw brings 1. 
+
+NOTE: Create a program to calculate and output the total points earned by the team
+"""
+print((18 * 3) + (7 * 1))
